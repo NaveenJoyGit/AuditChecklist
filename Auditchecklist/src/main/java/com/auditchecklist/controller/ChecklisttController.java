5f752c0a45ca1f016d8e63345670d9fd175c0f59
NaveenJoyGit <naveenjoy.mec@gmail.com>
2021-04-30 10:32:40 +0530

    final touches

--- Auditchecklist/src/main/java/com/auditchecklist/controller/ChecklisttController.java
+++ Auditchecklist/src/main/java/com/auditchecklist/controller/ChecklisttController.java
@@ -1,16 +1,12 @@
 package com.auditchecklist.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.auditchecklist.model.AuditType;
-import com.auditchecklist.model.Questions;
 import com.auditchecklist.services.AuditTypeServices;
-import com.auditchecklist.services.QuestionsServices;
 
 @RestController
 public class ChecklisttController {
